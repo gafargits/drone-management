b2f6d438acd12111be97f00d4296760d04c9bcb8
gafargits <rajigafaronline@gmail.com>
2021-12-17 18:24:28 +0100

    initial project setup

---
+++ src/test/java/com/musala/dronemanagement/DroneManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musala.dronemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
